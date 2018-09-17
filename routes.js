e95e0c977e6b824feb5794b78d00b6827b97329f
Matt Helm <mthelm85@gmail.com>
2018-09-17 06:31:32 -0500

    search case id as number, add zip code route

--- routes.js
+++ routes.js
@@ -2,7 +2,16 @@ module.exports = (app, db) => {
 
   app.get('/case-id', (req, res) => {
     db.collection('enforcement_data')
-    .find({ case_id: req.query.case_id })
+    .find({ "case_id": Number(req.query.case_id) })
+    .toArray((err, docs) => {
+      if (err) res.json({ error: err })
+      res.json({ results: docs })
+    })
+  })
+
+  app.get('/zip-code', (req, res) => {
+    db.collection('enforcement_data')
+    .find({ "zip_cd": Number(req.query.zip_cd) })
     .toArray((err, docs) => {
       if (err) res.json({ error: err })
       res.json({ results: docs })
